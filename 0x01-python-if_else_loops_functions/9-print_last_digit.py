42d1750f4a285c801fb8c2546a83c30ab31e6312
ConradMLinus <conradmlinus@gmail.com>
2023-11-28 09:35:30 -0800

    Correcting code

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
-    if number < 0:
-        last_digit = (number * -1) % 10
+    if(number >= 0):
+        remainder = number % 10
+        print(f"{remainder}", end='')
     else:
-        last_digit = number % 10
-    print("{}".format(last_digit), end="")
-    return(last_digit)
+        remainder = -(number % -10)
+        print(f"{remainder}", end='')
+    return (remainder)
