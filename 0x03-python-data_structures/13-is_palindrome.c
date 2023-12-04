69181b1c8bd2e65145025d676f5fcdc9c60b7be4
ConradMLinus <conradmlinus@gmail.com>
2023-12-04 11:04:47 -0800

    code update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,146 +1,72 @@
-i#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-/* Definition for singly-linked list */
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
-/* Function to reverse a linked list */
-listint_t *reverse(listint_t *head) {
-    listint_t *prev = NULL;
-    listint_t *current = head;
-    listint_t *next = NULL;
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
 
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    return (prev);
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
 }
 
-/* Function to check if the original and reversed linked lists are identical */
-int compareLists(listint_t *list1, listint_t *list2) {
-    while (list1 != NULL && list2 != NULL) {
-        if (list1->n != list2->n)
-            return (0);
-        list1 = list1->next;
-        list2 = list2->next;
-    }
-    return (list1 == NULL && list2 == NULL);
-}
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-/* Function to check if the linked list is a palindrome */
-int is_palindrome(listint_t **head) {
-    listint_t *slow_ptr = *head;
-    listint_t *fast_ptr = *head;
-    listint_t *second_half = NULL;
-    listint_t *prev_of_slow_ptr = *head;
-    listint_t *mid_node = NULL;
-    int res = 1; /*Result initially set as palindrome*/
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    if (*head != NULL && (*head)->next != NULL) {
-        /* Get the middle of the list */
-        while (fast_ptr != NULL && fast_ptr->next != NULL) {
-            fast_ptr = fast_ptr->next->next;
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
 
-            /* We need previous of the slow_ptr for linked lists with odd elements */
-            prev_of_slow_ptr = slow_ptr;
-            slow_ptr = slow_ptr->next;
-        }
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-        /* Even number of elements in the list, move slow_ptr to the second half */
-        if (fast_ptr != NULL) {
-            mid_node = slow_ptr;
-            slow_ptr = slow_ptr->next;
-        }
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
 
-        /* Reverse the second half and set it as the starting point */
-        second_half = slow_ptr;
-        prev_of_slow_ptr->next = NULL; /*Break the first half*/
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
 
-        second_half = reverse(second_half); /*Reverse the second half*/
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
 
-        /* Compare first and second halves */
-        res = compareLists(*head, second_half);
-
-        /* Construct the original list back and restore the links */
-        second_half = reverse(second_half);
-
-        if (mid_node != NULL) { /*If odd elements, reconnect the lists*/
-            prev_of_slow_ptr->next = mid_node;
-            mid_node->next = second_half;
-        } else {
-            prev_of_slow_ptr->next = second_half;
-        }
-    }
-    return (res);
-}
-
-/* Function to insert a new node at the beginning of the linked list */
-void insert_at_beginning(listint_t **head, int new_data) {
-    listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-    if (new_node == NULL) {
-        printf("Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = new_data;
-    new_node->next = *head;
-    *head = new_node;
-}
-
-/* Function to print the linked list */
-void print_list(listint_t *node) {
-    while (node != NULL) {
-        printf("%d ", node->n);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-/* Function to delete the linked list */
-void delete_list(listint_t **head) {
-    listint_t *current = *head;
-    listint_t *next;
-
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-
-    *head = NULL;
-}
-
-/* Main function for testing */
-int main() {
-    listint_t *head = NULL;
-    int palindrome_result;
-
-    /* Insert elements into the linked list */
-    insert_at_beginning(&head, 1);
-    insert_at_beginning(&head, 2);
-    insert_at_beginning(&head, 3);
-    insert_at_beginning(&head, 2);
-    insert_at_beginning(&head, 1);
-
-    printf("Linked list: ");
-    print_list(head);
-
-    palindrome_result = is_palindrome(&head);
-
-    if (palindrome_result == 1) {
-        printf("The linked list is a palindrome\n");
-    } else {
-        printf("The linked list is not a palindrome\n");
-    }
-
-    /* Delete the linked list */
-    delete_list(&head);
-
-    return (0);
+	return (1);
 }
